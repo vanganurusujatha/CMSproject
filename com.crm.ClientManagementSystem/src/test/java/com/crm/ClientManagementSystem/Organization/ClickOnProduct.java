5c5304d4243eaed2ce659c0e11ede981f02c773c
sujat <sujat@Sujatha>
2026-04-20 13:38:55 +0530

    Sujatha second commit

---
+++ com.crm.ClientManagementSystem/src/test/java/com/crm/ClientManagementSystem/Organization/ClickOnProduct.java
@@ -0,0 +1,12 @@
+package com.crm.ClientManagementSystem.Organization;
+
+import org.testng.annotations.Test;
+
+import com.crm.ClientManagementSystem.GenericLibrary.Baseclass;
+
+public class ClickOnProduct extends Baseclass {
+	@Test
+	public void products() {
+
+}
+}
